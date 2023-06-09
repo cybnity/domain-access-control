e867a3c36f7335214be9d95e46124ee904fb613b
Olivier Lemée <olivier@agnet.fr>
2023-06-09 11:07:24 +0200

    refactoring

--- implementation-line/access-control/ac-domain-model/src/test/java/org/cybnity/accesscontrol/domain/model/sample/writemodel/OrganizationProperty.java
+++ implementation-line/access-control/ac-domain-model/src/test/java/org/cybnity/accesscontrol/domain/model/sample/writemodel/OrganizationProperty.java
@@ -2,7 +2,9 @@ package org.cybnity.accesscontrol.domain.model.sample.writemodel;
 
 import java.io.Serializable;
 import java.time.OffsetDateTime;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.cybnity.framework.immutable.Entity;
 import org.cybnity.framework.immutable.HistoryState;
@@ -23,81 +25,81 @@ import org.cybnity.framework.immutable.utility.VersionConcreteStrategy;
  */
 public class OrganizationProperty extends MutableProperty {
 
-    private static final long serialVersionUID = 1L;
-    private OffsetDateTime versionedAt;
+	private static final long serialVersionUID = 1L;
+	private OffsetDateTime versionedAt;
 
-    /**
-     * Example of keys set regarding the multiple attribute defining this complex
-     * organization, and that each change need to be versioned/treated as a single
-     * atomic fact.
-     */
-    public enum PropertyAttributeKey {
-	Name, LocationCity, LocationCountry;
-    }
+	/**
+	 * Example of keys set regarding the multiple attribute defining this complex
+	 * organization, and that each change need to be versioned/treated as a single
+	 * atomic fact.
+	 */
+	public enum PropertyAttributeKey {
+		Name, LocationCity, LocationCountry;
+	}
 
-    public OrganizationProperty(Entity propertyOwner, HashMap<String, Object> propertyCurrentValue,
-	    HistoryState status) throws IllegalArgumentException {
-	super(propertyOwner, propertyCurrentValue, status);
-	this.versionedAt = OffsetDateTime.now();
-    }
+	public OrganizationProperty(Entity propertyOwner, HashMap<String, Object> propertyCurrentValue, HistoryState status)
+			throws IllegalArgumentException {
+		super(propertyOwner, propertyCurrentValue, status);
+		this.versionedAt = OffsetDateTime.now();
+	}
 
-    public OrganizationProperty(Entity propertyOwner, HashMap<String, Object> propertyCurrentValue,
-	    HistoryState status, OrganizationProperty... prior) throws IllegalArgumentException {
-	super(propertyOwner, propertyCurrentValue, status, prior);
-	this.versionedAt = OffsetDateTime.now();
-    }
+	public OrganizationProperty(Entity propertyOwner, HashMap<String, Object> propertyCurrentValue, HistoryState status,
+			OrganizationProperty... prior) throws IllegalArgumentException {
+		super(propertyOwner, propertyCurrentValue, status, prior);
+		this.versionedAt = OffsetDateTime.now();
+	}
 
-    /**
-     * Get the name of the organization.
-     * 
-     * @return A label or null.
-     */
-    public String getOrganizationName() {
-	return (String) this.currentValue().getOrDefault(PropertyAttributeKey.Name.name(), null);
-    }
+	/**
+	 * Get the name of the organization.
+	 * 
+	 * @return A label or null.
+	 */
+	public String getOrganizationName() {
+		return (String) this.currentValue().getOrDefault(PropertyAttributeKey.Name.name(), null);
+	}
 
-    @Override
-    public OffsetDateTime occurredAt() {
-	return this.versionedAt;
-    }
+	@Override
+	public OffsetDateTime occurredAt() {
+		return this.versionedAt;
+	}
 
-    @Override
-    public Serializable immutable() throws ImmutabilityException {
-	OrganizationProperty copy = new OrganizationProperty(this.owner(), this.currentValue(),
-		this.historyStatus());
-	// Complete with additional attributes of this complex property
-	copy.versionedAt = this.versionedAt;
-	copy.changedAt = this.occurredAt();
-	copy.updateChangesHistory(this.changesHistory());
-	return copy;
-    }
+	@Override
+	public Serializable immutable() throws ImmutabilityException {
+		OrganizationProperty copy = new OrganizationProperty(this.owner(),
+				new HashMap<String, Object>(this.currentValue()), this.historyStatus());
+		// Complete with additional attributes of this complex property
+		copy.versionedAt = this.versionedAt;
+		copy.changedAt = this.occurredAt();
+		copy.updateChangesHistory(this.changesHistory());
+		return copy;
+	}
 
-    /**
-     * Get the current value of this complex property.
-     * 
-     * @return A set of valued attributes.
-     */
-    public HashMap<String, Object> currentValue() {
-	return this.value;
-    }
+	/**
+	 * Get the current value of this complex property.
+	 * 
+	 * @return A set of valued attributes.
+	 */
+	public Map<String, Object> currentValue() {
+		return Collections.unmodifiableMap(this.value);
+	}
 
-    /**
-     * Who is the owner of this property
-     * 
-     * @return The owner
-     * @throws ImmutabilityException If impossible creation of immutable version of
-     *                               instance
-     */
-    public Entity owner() throws ImmutabilityException {
-	return (Entity) this.owner.immutable();
-    }
+	/**
+	 * Who is the owner of this property
+	 * 
+	 * @return The owner
+	 * @throws ImmutabilityException If impossible creation of immutable version of
+	 *                               instance
+	 */
+	public Entity owner() throws ImmutabilityException {
+		return (Entity) this.owner.immutable();
+	}
 
-    /**
-     * Implement the generation of version hash regarding this class type according
-     * to a concrete strategy utility service.
-     */
-    @Override
-    public String versionHash() {
-	return new VersionConcreteStrategy().composeCanonicalVersionHash(getClass());
-    }
+	/**
+	 * Implement the generation of version hash regarding this class type according
+	 * to a concrete strategy utility service.
+	 */
+	@Override
+	public String versionHash() {
+		return new VersionConcreteStrategy().composeCanonicalVersionHash(getClass());
+	}
 }
