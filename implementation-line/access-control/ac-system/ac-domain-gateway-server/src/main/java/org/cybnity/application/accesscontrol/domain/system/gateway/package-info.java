531ce47817d36c51400ee2ed84b5114301346b73
Olivier Lemée <olivier@agnet.fr>
2023-12-08 15:41:50 +0100

    Update package-info.java

--- implementation-line/access-control/ac-system/ac-domain-gateway-server/src/main/java/org/cybnity/application/accesscontrol/domain/system/gateway/package-info.java
+++ implementation-line/access-control/ac-system/ac-domain-gateway-server/src/main/java/org/cybnity/application/accesscontrol/domain/system/gateway/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Server module supporting the RTS features and services.
+ * Application module as domain API facade.
  * @author olivier
  */
 package org.cybnity.application.accesscontrol.domain.system.gateway;
