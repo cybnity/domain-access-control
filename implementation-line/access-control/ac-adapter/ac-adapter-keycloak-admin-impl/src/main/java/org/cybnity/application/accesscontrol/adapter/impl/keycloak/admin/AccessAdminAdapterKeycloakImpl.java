0a3797a6bd1c9c4c11d81670e21d9ff0c959394c
Olivier Lemée <olivier@agnet.fr>
2026-06-26 19:09:14 +0200

    connector to Keycloak Admin client api
    - supporting access token read, expiration date management
    - supporting integration test plan for API authentication over Rest API

--- implementation-line/access-control/ac-adapter/ac-adapter-keycloak-admin-impl/src/main/java/org/cybnity/application/accesscontrol/adapter/impl/keycloak/admin/AccessAdminAdapterKeycloakImpl.java
+++ implementation-line/access-control/ac-adapter/ac-adapter-keycloak-admin-impl/src/main/java/org/cybnity/application/accesscontrol/adapter/impl/keycloak/admin/AccessAdminAdapterKeycloakImpl.java
@@ -10,8 +10,11 @@ import org.cybnity.framework.UnoperationalStateException;
 import org.cybnity.keycloak.domain.model.Realm;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.ServerInfoResource;
+import org.keycloak.admin.client.token.TokenManager;
+import org.keycloak.representations.AccessTokenResponse;
 
+import java.time.OffsetDateTime;
 import java.util.logging.Logger;
 
 /**
@@ -42,6 +45,11 @@ public class AccessAdminAdapterKeycloakImpl implements IAccessAdminAdapter {
     private Keycloak keycloakAdminClient;
 
     /**
+     * Limit date of expiration of the latest token provided by Keycloak server.
+     */
+    private OffsetDateTime currentTokenExpiringAt;
+
+    /**
      * Default constructor of the adapter ready to manage remote interactions with a Keycloak instance(s).
      *
      * @param context Mandatory context provider of reusable configuration allowing
@@ -62,9 +70,9 @@ public class AccessAdminAdapterKeycloakImpl implements IAccessAdminAdapter {
         // Test connection to Keycloak instance over the Admin REST API client library
         // See https://www.keycloak.org/securing-apps/admin-client documentation about Keycloak admin client usage
         try {
-            RealmResource realmResource = getKeycloakAdminClient(context).realm(context.get(AdminConfigurationVariable.REALM_MASTER_NAME));
-            if (realmResource == null || realmResource.toRepresentation() == null)
-                throw new UnoperationalStateException(AdminConfigurationVariable.REALM_MASTER_NAME + " resource not found in Keycloak server!");
+            ServerInfoResource res = getKeycloakAdminClient(context).serverInfo();
+            if (res == null || res.getInfo() == null)
+                throw new UnoperationalStateException(" Server info resource not found in Keycloak server!");
         } catch (Exception e) {
             throw new UnoperationalStateException(e);
         }
@@ -73,7 +81,9 @@ public class AccessAdminAdapterKeycloakImpl implements IAccessAdminAdapter {
     @Override
     public void freeUpResources() {
         // Disconnect Keycloak REST API stub is existing
-        if (keycloakAdminClient != null) keycloakAdminClient.close();
+        if (keycloakAdminClient != null) {
+            keycloakAdminClient.close();
+        }
     }
 
     @Override
@@ -97,7 +107,7 @@ public class AccessAdminAdapterKeycloakImpl implements IAccessAdminAdapter {
         if (keycloakAdminClient != null) {
             // Check if previous connector is instantiated and is operational (e.g; Keycloak instance have not been undeployed or client decommissioned which shall be re-established
             if (!keycloakAdminClient.isClosed()) {
-                // TODO Evaluate if potential token expiration of previous client instance is expired (and need to be refreshed before to return operational client) or is not granted (e.g; changed account user name or password in Keycloak server-side web console requiring to instantiate new client based on new authentication account from environment variable normally also upgraded)
+                manageTokenUpdate(keycloakAdminClient); // Manage eventual access token expiration requiring its refresh for new period
                 return keycloakAdminClient;
             }
         }
@@ -107,6 +117,9 @@ public class AccessAdminAdapterKeycloakImpl implements IAccessAdminAdapter {
 
         // Create or re-instantiate singleton instance to Keycloak server over its administration API client
         // See https://www.keycloak.org/securing-apps/admin-client documentation
+
+        // See admin client library source code project at https://github.com/keycloak/keycloak-client/blob/main/admin-client/src/main/java/org/keycloak/admin/client/Keycloak.java
+        // the builder implementation uses a default RestEasy client builder settings
         keycloakAdminClient = KeycloakBuilder.builder()
                 // Read current up-to-date administration client configuration allowing Keycloak Admin Client API usage of master realm (supporting any environment configuration HOT changes)
                 .serverUrl(context.get(AdminConfigurationVariable.KEYCLOAK_SERVER_URL))
@@ -116,7 +129,71 @@ public class AccessAdminAdapterKeycloakImpl implements IAccessAdminAdapter {
                 .username(context.get(AdminConfigurationVariable.REALM_MASTER_USERNAME))
                 .password(context.get(AdminConfigurationVariable.REALM_MASTER_PASSWORD))
                 .build();
-        return keycloakAdminClient;
+
+        // Initialize an original access token for this client, which is usable during a period (defined in Keycloak server)
+        manageTokenUpdate(keycloakAdminClient);
+
+        return keycloakAdminClient; // return operational client ready for use
+    }
+
+    /**
+     * Define the current token expiring date based from access token provided by Keycloak Admin Client API library.
+     * This method is synchronized on the remote call to Keycloak. When the expiration date of the current token is not reached, the Keycloak Admin Client only return the current period before next expiration.
+     * But if Keycloak Admin Client lib identify that a expiration date is reached, it makes a call to Keycloak server for received a new refresh token available for a new period.
+     * The currentTokenExpiringAt attribute is upgraded automatically for the new date of future expiration as limit for the usable token.
+     *
+     * @param adminClient Mandatory client.
+     * @throws IllegalArgumentException    When mandatory parameter is missing.
+     * @throws UnoperationalStateException When any exception occurred by Keycloak Admin Client library, or during call to Keycloak server.
+     */
+    private void manageTokenUpdate(Keycloak adminClient) throws IllegalArgumentException, UnoperationalStateException {
+        if (adminClient == null) throw new IllegalArgumentException("adminClient parameter is required!");
+        try {
+            boolean requiredExpirationLimitRefresh = false;
+
+            // Check if latest provided token is not expired and should be refreshed before to return the client instance
+            // Evaluate if potential token expiration of previous client instance is expired (and need to be refreshed before to return operational client)
+            // or is not granted (e.g; changed account user name or password in Keycloak server-side web console requiring to instantiate new client based on new authentication account from environment variable normally also upgraded)
+            if (currentTokenExpiringAt != null) {
+                // Verify if expiration date is reached justifying call to received refresh token
+                // Optimization rule: Potential duration of this SYNCHRONIZED CALL TO KEYCLOAK SERVER BY ADMIN CLIENT!
+                if (!/* past fact */ currentTokenExpiringAt.isAfter(OffsetDateTime.now()) /* no more time for token usage */) {
+                    requiredExpirationLimitRefresh = true;
+                }
+                // Else none new call requested because the client can continue to use the current access token currently in the expiration period not reached
+            } else {
+                // None initial date have been allowed by Keycloak (e.g; none access token have been originally received by Keycloak Admin client lib
+                requiredExpirationLimitRefresh = true;
+            }
+            if (requiredExpirationLimitRefresh) {
+                // Identify initial date of expiration based on current access token
+                // Request current access token from token manager, that will be automatically managed (automatically refreshed by TokenManager when expire date reached)
+                TokenManager tokenMgt = adminClient.tokenManager();
+                AccessTokenResponse accessToken = tokenMgt.getAccessToken(); // Potential duration of this synchronized call! If previous original access token expired, a refresh is automatically executed by the token manager for allow continuity of the client
+                long tokenExpiredInSeconds = accessToken.getExpiresIn(); // Duration in seconds before end of validity
+                // Identify date when refresh of token will be required
+                currentTokenExpiringAt = defineExpirationLimitDate(OffsetDateTime.now() /* from now */, tokenExpiredInSeconds /* max duration in seconds */);
+            }
+        } catch (Exception e) {
+            throw new UnoperationalStateException(e);
+        }
+    }
+
+    /**
+     * Calculate and return an expiration date based on a date including a limitation duration in seconds.
+     *
+     * @param since          Mandatory origin date.
+     * @param afterInSeconds Duration in seconds. When null, since date parameter is returned.
+     * @return Calculated date of expiration.
+     * @throws IllegalArgumentException When mandatory parameter is missing.
+     */
+    private OffsetDateTime defineExpirationLimitDate(OffsetDateTime since, Long afterInSeconds) throws IllegalArgumentException {
+        if (since == null) throw new IllegalArgumentException("since parameter is required!");
+        if (afterInSeconds != null) {
+            // Add duration in seconds (or reduce duration from origin data if negative
+            return since.plusSeconds(afterInSeconds);
+        }
+        return since;
     }
 
     @Override
